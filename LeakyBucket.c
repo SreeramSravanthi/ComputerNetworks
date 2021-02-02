8e239258c1e0921398e48a1ecbf84a9a23598372
SreeramSravanthi <51986326+SreeramSravanthi@users.noreply.github.com>
2021-02-02 11:27:59 +0530

    Add files via upload

---
+++ LeakyBucket.c
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define bucketSize 512
+
+void bktInput(int pktSize,int outRate){
+    if(pktSize > bucketSize){
+        printf("Bucket overflow\n");
+    }
+    else {
+        while(pktSize > outRate){
+            printf("%d bytes outputed\n",outRate);
+            pktSize -= outRate;
+        }
+    
+        if(pktSize > 0){
+            printf("Last %d bytes sent\n" ,pktSize);
+        }
+        printf("Bucket output successful\n");
+    }
+}
+
+int main(){
+    int outRate, pktSize;
+    srand(time(0));
+    printf("Enter output rate : ");
+    scanf("%d",&outRate);
+    for(int i=1;i<=5;i++){
+        pktSize = rand() % 600;
+        printf("Packet no %d",i);
+        printf("\t Packet size = %d\n",pktSize);
+        bktInput(pktSize,outRate);
+    }
+}
